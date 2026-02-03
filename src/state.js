ebeb86d61d63275159f4fa1bfb4716474ee9122b
suchithrkar <suchith.rkar@gmail.com>
2026-02-03 10:28:10 +0530

    Wire remaining processing actions

---
+++ src/state.js
@@ -0,0 +1,19 @@
+window.appState = {
+  kciExcel: {
+    file: null,
+    raw: {},
+    normalized: {},
+  },
+  csoCsv: {
+    file: null,
+    data: [],
+  },
+  trackingCsv: {
+    file: null,
+    data: [],
+  },
+  repairCases: [],
+  closedCases: [],
+  copySoOrders: [],
+  copyTrackingUrls: [],
+};
