2d742c03c6e7b7c51d89e8dfa4bc3768c03d5914
suchithrkar <suchith.rkar@gmail.com>
2026-02-03 10:14:00 +0530

    Add repair cases engine

---
+++ src/state.js
@@ -0,0 +1,14 @@
+window.appState = {
+  kciExcelFile: null,
+  kciExcelData: {},
+  csoCsv: {
+    file: null,
+    data: [],
+  },
+  trackingCsv: {
+    file: null,
+    data: [],
+  },
+  repairCases: [],
+  closedCases: [],
+};
